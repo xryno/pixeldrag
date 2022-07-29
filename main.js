f930e03145b7efdca2f33fcffe3c2ba0e63a4702
rcurry7 <rcurry87@gmail.com>
2022-07-29 13:08:54 +0100

    1st

---
+++ main.js
@@ -0,0 +1,459 @@
+let garage = new Audio("audio/title.mp3");
+let wheelspin = new Audio("audio/wheelspin.mp3");
+let beep = new Audio("audio/beep.wav");
+let go = new Audio("audio/go.wav");
+let drive = new Audio("audio/drive.mp3");
+let path = "audio/rx7engine.wav";
+
+
+
+let player = document.getElementById("player");
+let wheel1 = document.getElementById("wheel1");
+let wheel2 = document.getElementById("wheel2");
+let revcounter = document.getElementById("revcounter");
+let needle = document.getElementById("needle");
+let needle2 = document.getElementById("revBarNeedle");
+let reactionTime = 0;
+let finalTime = 0;
+let nameBox = document.getElementById("inputName");
+let scoreNameArray = [document.getElementById("score1"), 
+                    document.getElementById("score2"), 
+                    document.getElementById("score3"), 
+                    document.getElementById("score4"), 
+                    document.getElementById("score5")]
+
+let scoreArray = [["Moomin: ", 2.815],
+                  ["Timmy: ", 5.612],
+                  ["Karen: ", 5.713],
+                  ["Pedro: ", 16.895],
+                  ["Bert: ", 17.126]]
+let keyProtect = 0
+let keyProtectRev = 0
+let launchRev = getCurrentRotation(needle);
+let launchSpeed = 0
+let launchMessage = document.getElementById("launchMessage");
+
+
+let revBy = 0;
+let revDownBy = -45;
+let activeCar = 0; //for object function to call correct player image
+
+let carGarage = [{
+  Make: "Mazda",
+  Model: "RX-7",
+  Year: "1998",
+  Power: "260 BHP",
+  image: function(){
+    document.getElementById("carImage").style.backgroundImage = "url(images/rx7.png)";
+    player.style.backgroundImage = "url(images/rx7.png)";
+    document.getElementById("wheel1").style.backgroundImage = "url(images/rx7wheel.png)";
+    document.getElementById("wheel2").style.backgroundImage = "url(images/rx7wheel.png)";
+    document.getElementById("wheel1").style.left = "2.2em";
+    document.getElementById("wheel2").style.left = "9.4em";
+    activeCar = 0;
+    path = "audio/rx7engine.wav"
+    setPath();
+        }
+},
+
+{
+  Make: "Nissan",
+  Model: "Skyline",
+  Year: "1994",
+  Power: "280 BHP",
+  image: function(){
+     document.getElementById("carImage").style.backgroundImage = "url(images/skyline.png)";
+     player.style.backgroundImage = "url(images/skyline.png)";
+     document.getElementById("wheel1").style.backgroundImage = "url(images/skylinewheel.png)";
+    document.getElementById("wheel2").style.backgroundImage = "url(images/skylinewheel.png)";
+    document.getElementById("wheel1").style.left = "1.75em";
+    document.getElementById("wheel2").style.left = "9.45em";
+     activeCar = 1;
+     path = "audio/skylineengine.wav"
+     setPath();
+     
+       }
+},
+
+{
+  Make: "Toyota",
+  Model: "Supra",
+  Year: "2020",
+  Power: "360 BHP",
+  image: function(){
+    document.getElementById("carImage").style.backgroundImage = "url(images/supra.png)";
+    player.style.backgroundImage = "url(images/supra.png)";
+    document.getElementById("wheel1").style.backgroundImage = "url(images/suprawheel.png)";
+    document.getElementById("wheel2").style.backgroundImage = "url(images/suprawheel.png)";
+    document.getElementById("wheel1").style.left = "2.1em";
+    document.getElementById("wheel2").style.left = "9.2em";
+    activeCar = 2;
+    path = "audio/supraengine.wav"
+    setPath();
+      }
+  
+}, 
+
+];
+
+function changeToGarage(){
+  garage.play();
+  garage.volume = 0.1;
+  changeCar(0);
+  document.getElementById("fadeScreen").style.display = "block";
+  document.getElementById("startScreen").style.display = "none";
+  document.getElementById("carSelect").style.display = "block";
+  document.getElementById("gameOver").style.display = "none";
+  document.getElementById("timerID").style.display = "none";
+  document.getElementById("highScores").style.display = "none";
+  // bgaudio.pause();
+  // garageaudio.play()
+  // garageaudio.volume = 0.40;
+  
+}
+                 
+
+ function changeCar(i) {              
+  let carSpecs = "";
+  for (let key in carGarage[i]) {
+    if([key].toString() !== "image"){
+      
+      carSpecs += '<li>'+ [key] + ": " +  carGarage[i][key] + '</li>' + '</br>';
+    }
+             }
+                          carGarage[i].image()
+  document.getElementById("carData").innerHTML  = carSpecs;
+
+    
+}
+
+
+ //start drag countdown 
+ function startRace () {
+  pitchChange = 0;
+  launchRev = 0;
+console.log(path)
+  revCar();
+ garage.pause();
+  keyProtect = 13;
+  keyProtectRev = 32;
+  launchSpeed = 0;
+  // carGarage[activeCar].image()
+  startNum = 5
+document.getElementById("fadeScreen").style.display = "none";
+document.getElementById("carSelect").style.display = "none";
+document.getElementById("highScores").style.display = "none";
+document.getElementById("startScreen").style.display = "none";
+document.getElementById("inputName").value = "";
+document.getElementById("timerID").style.display = "block";
+document.getElementById("gameOver").style.display = "none";
+document.getElementById("startBtn").style.display = "none";
+document.getElementById("jumpStart").style.display = "none";
+document.getElementById("revcounter").style.display = "block";
+document.getElementById("needle").style.display = "block";
+document.getElementById("timeR").textContent = "0.00";
+needle.style.transform = "rotate(-45deg)";
+player.style.setProperty("animation", "0");
+wheel1.style.setProperty("animation", "0");
+wheel2.style.setProperty("animation", "0");
+
+let amberLights = document.querySelectorAll('[class^="amber"]');
+
+for (let i = 0; i < amberLights.length; i++) {
+amberLights[i].style.background = "rgb(255, 217, 0)";
+amberLights[i].style.boxShadow = "0em 0em 2em 0em yellow";
+}
+
+player.style.display = "block";
+
+countDown()
+
+}
+
+
+
+
+
+//get rotation point of needle
+
+  function getCurrentRotation(el){
+  let style = window.getComputedStyle(el);
+  let transF = style.getPropertyValue("transform")
+  let values = transF.split('(')[1].split(')')[0].split(',');
+  return Math.round(Math.atan2(values[1],values[0]) * (180/Math.PI));
+  }
+
+
+//rev down
+
+document.body.addEventListener('keyup', keyUpPressesUp);    
+function keyUpPressesUp(press) {
+
+  if(press.keyCode === keyProtectRev) {
+    player.style.setProperty("animation", "0");
+    revBy = 0;
+    needle.style.transform = "rotate("  + revDownBy + "deg)";
+    pitchChange = -0.04;
+  }            
+        
+}
+ 
+//rev up
+
+document.body.addEventListener('keydown', keyUpPresses);    
+function keyUpPresses(press) {
+         
+  if(press.keyCode === keyProtectRev) {
+    player.style.setProperty("animation", "shake 0.4s");
+    revBy += 28;
+    needle.style.transform = "rotate(" + revBy + "deg)";
+    launchRev = getCurrentRotation(needle);
+    pitchChange = 0.02
+  }
+
+
+  //launch
+
+    if(press.keyCode === keyProtect) {
+
+      if (startNum > 0){
+        jumpStart ();
+      } else {
+        
+        wheelspin.volume = 0.5;
+        wheelspin.play();
+        drive.volume = 0.3;
+        drive.play();
+        keyProtectRev = 0;
+        clearInterval(counttimer);
+        reactionTime = startNum / 1.5;
+        launchSpeed = launchRev
+    
+     
+
+  //launch quality
+
+        if (launchSpeed > 70 && launchSpeed < 115) {
+          player.style.setProperty("animation", "launch 2s ease-in");
+          launchMessage.style.display = "block";
+          launchMessage.innerText = "Perfect launch!"
+        } else if (launchSpeed > 30 && launchSpeed < 70) {
+          player.style.setProperty("animation", "launch 2.1s ease-in");
+          launchMessage.style.display = "block";
+          launchMessage.innerText = "Good launch!"
+        } else {
+          player.style.setProperty("animation", "launch 2.5s ease-in");
+          launchMessage.style.display = "block";
+          launchMessage.innerText = "Poor launch!"
+        }
+        
+        wheel1.style.setProperty("animation", "wheels 0.08s ease-out infinite");
+        wheel2.style.setProperty("animation", "wheels 0.1s linear infinite");
+        document.querySelector('#lightContainer :nth-child(7)').style.background = "none";
+        document.querySelector('#lightContainer :nth-child(8)').style.background = "none";
+        document.querySelector('#lightContainer :nth-child(7)').style.boxShadow = "none";
+        document.querySelector('#lightContainer :nth-child(8)').style.boxShadow = "none";
+        document.getElementById("revcounter").style.display = "none";
+        document.getElementById("needle").style.display = "none";
+
+          startTimer()
+      }
+    }
+    }
+
+  
+
+
+
+    function startTimer() {
+        starttimer = setInterval(function () {
+          let timeLeft = document.getElementById("timeR").textContent;
+          let current = parseFloat(timeLeft) + 0.01;
+          document.getElementById("timeR").textContent = current.toFixed(2);
+          
+        }, 10);
+      } 
+      
+
+      let startNum = 5
+     
+
+      function countDown() {
+        counttimer = setInterval(function () {
+           
+        let current = startNum - 0.01;
+        startNum = current
+
+        if (startNum > 3.9 && startNum < 4.3){
+          beep.play()
+        }
+
+        if (startNum < 4){
+
+
+          
+          
+          document.querySelector('#lightContainer :nth-child(1)').style.background = "none";
+          document.querySelector('#lightContainer :nth-child(1)').style.boxShadow = "none";
+          document.querySelector('#lightContainer :nth-child(2)').style.background = "none";
+          document.querySelector('#lightContainer :nth-child(2)').style.boxShadow = "none";
+            
+ 
+        }
+       
+         
+      
+        if (startNum < 3){
+          
+          
+          document.querySelector('#lightContainer :nth-child(3)').style.background = "none";
+          document.querySelector('#lightContainer :nth-child(3)').style.boxShadow = "none";
+          document.querySelector('#lightContainer :nth-child(4)').style.background = "none";
+          document.querySelector('#lightContainer :nth-child(4)').style.boxShadow = "none";
+
+        }
+
+        if (startNum < 2){
+          
+          document.querySelector('#lightContainer :nth-child(5)').style.background = "none";
+          document.querySelector('#lightContainer :nth-child(5)').style.boxShadow = "none";
+          document.querySelector('#lightContainer :nth-child(6)').style.background = "none";
+          document.querySelector('#lightContainer :nth-child(6)').style.boxShadow = "none";
+
+      }
+
+      if (startNum > 0.01 && startNum < 0.02 ){
+        go.play()
+      }
+        if (startNum < 0){
+          
+         
+          document.querySelector('#lightContainer :nth-child(7)').style.background = "green";
+          document.querySelector('#lightContainer :nth-child(7)').style.boxShadow = "0em 0em 2em 0em rgb(234, 255, 2)";
+          document.querySelector('#lightContainer :nth-child(8)').style.background = "green";
+          document.querySelector('#lightContainer :nth-child(8)').style.boxShadow = "0em 0em 2em 0em rgb(234, 255, 2)";
+        }
+
+        if (startNum < -5){
+          
+      }
+        
+        }, 10);         
+      } 
+
+
+      setInterval (function(){
+
+//rev limiter
+        if (launchRev > 150 || launchRev < -46) {
+          needle.style.transform = "rotate("  + revDownBy + "deg)";
+        } 
+
+       
+//check if crossed line
+
+        let carLeft = parseInt(window.getComputedStyle(player).getPropertyValue("left"));
+        if (carLeft >= 920) {
+          //get dragtime 
+         finalTime = parseFloat(document.getElementById("timeR").textContent) + Math.abs(reactionTime) 
+        }
+        if (carLeft >= 970) {
+         
+          gameOver();
+        } 
+        
+        }, 1);     
+
+
+
+
+ function gameOver (){
+
+
+  clearInterval(starttimer);
+  document.getElementById("timeR").textContent = "0.00"
+   document.getElementById("gameOver").style.display = "block";
+   document.getElementById("timerID").style.display = "none";
+   launchMessage.style.display = "none";
+   player.style.display = "none";
+   yourReact.textContent = `Reaction: ${Math.abs(reactionTime.toFixed(3))}`;
+   yourReact2.textContent = `Reaction: ${Math.abs(reactionTime.toFixed(3))}`;
+   yourScore.textContent = `Your time: ${finalTime.toFixed(4)}`;
+ 
+   if (finalTime < scoreArray[0][1]){
+     document.getElementById("newHighScore").style.display = "block";
+     document.getElementById("newHigh").textContent = `Your time: ${finalTime.toFixed(3)}`;
+     document.getElementById("gameOver").style.display = "none";
+    }
+   }
+
+   function jumpStart (){
+    clearInterval(counttimer);
+    document.getElementById("timeR").textContent = "0.00"
+     document.getElementById("jumpStart").style.display = "block";
+     document.getElementById("timerID").style.display = "none";
+     player.style.display = "none";
+     }
+
+
+  function highScores (){   
+    document.getElementById("newHighScore").style.display = "none";
+    document.getElementById("highScores").style.display = "block";
+    document.getElementById("gameOver").style.display = "none;";
+  
+    if (finalTime < scoreArray[0][1]){
+      scoreArray.pop();
+      scoreArray.unshift([nameBox.value, finalTime.toFixed(3)])
+    }
+    for (let i = 0; i<5; i++){
+      scoreNameArray[i].textContent = scoreArray[i][0] + ": " + scoreArray[i][1]; //assigns array values to scoreboard
+    }
+  }
+  
+//rev sound effect
+  
+       let audio;
+       let pitchChange = 0;
+       let revChangeTimer;
+       const audioCtx = new (AudioContext)()
+    //using html web audio api
+    function setPath(){
+       // makes new audio context obj
+      fetch(path)  //set path of sound file
+      .then(data  => data.arrayBuffer()) //puts sound data into buffer - 
+      .then(arrayBuffer => audioCtx.decodeAudioData(arrayBuffer))  //decodes sound data
+      .then(decodedAudio => {
+          audio = decodedAudio;     //assign decoded sound to audio var - can now play with it
+      });
+    }
+      let playSound = audioCtx.createBufferSource(); //make buffer source
+
+      function revCar (){
+        playSound = audioCtx.createBufferSource();
+        playSound.buffer = audio;                        //assign decoded sound to buffer source
+        playSound.connect(audioCtx.destination);        //connect sound to audio ctx
+        playSound.loop = true;                          //loop the sound
+        playSound.start(audioCtx.currentTime);          //play sound
+
+
+        revChangeTimer = setInterval(function(){
+          let currPitch = playSound.playbackRate.value;     //current pitch
+             
+          if ((pitchChange < 0 && currPitch > 1.0) ||(pitchChange > 0 && currPitch < 2.2)) {
+            playSound.playbackRate.value += pitchChange;  //change playback rate depending on keyup/down value at time.
+          }
+        }, 35);
+       } 
+
+    window.addEventListener("keydown", stopSound);
+    function stopSound(press) {
+
+          if (press.keyCode == keyProtect) {
+            if(playSound !== null) {
+            playSound.stop();
+            playSound = null;  //needed to reinstate audio when restart
+            clearInterval(revChangeTimer); 
+            keyProtect = 0;
+            }
+          }
+        };
